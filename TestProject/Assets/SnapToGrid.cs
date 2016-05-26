bd9534ac4a406237afee1e68c91221e57640e363
Uriel <leroymachelle@gmail.com>
2016-05-25 22:13:56 -0700

    Added Random Generation

---
+++ TestProject/Assets/SnapToGrid.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnapToGrid : MonoBehaviour {
+
+	public int gridSize;
+
+	void Update () {
+		transform.position = new Vector3 (Mathf.Floor(transform.position.x / gridSize) * gridSize, 
+			Mathf.Floor(transform.position.y / gridSize) * gridSize, transform.position.z);
+	}
+}
