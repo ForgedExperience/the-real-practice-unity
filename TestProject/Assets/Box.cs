622363e1595c7c7af1a45db937d8a58f466d37bd
samuelwilson483103 <samuelwilson483103@gmail.com>
2016-05-25 22:16:01 -0700

    added movement and raycast

--- TestProject/Assets/Box.cs
+++ TestProject/Assets/Box.cs
@@ -2,15 +2,48 @@
 using System.Collections;
 
 public class Box : MonoBehaviour {
+	private Rigidbody rgbd;
+	private RaycastHit hit;
+	private Vector3 down;
+	public float distance;
+	private bool grounded;
+	public float speed= 100;
+	void Start (){
+		down = transform.TransformDirection (Vector3.down);
+		rgbd = GetComponent<Rigidbody> ();
+	}
+	void FixedUpdate (){
+		
+		if (Physics.Raycast (transform.position, down, out hit,1)) {
+			grounded = true;
 
-	// Use this for initialization
-	void Start () {
-	
+		} else {
+			grounded = false;
+		
+		} 
+
+
+		Debug.DrawRay (transform.position, down, Color.blue);
+		if (Input.GetKey (KeyCode.W) && grounded == true) {
+			rgbd.AddForce (0, 0, speed);
+		
+		}
+
+		if (Input.GetKey (KeyCode.S) && grounded == true) {
+			rgbd.AddForce (0, 0, -speed);
+
+		}if (Input.GetKey (KeyCode.A) && grounded == true) {
+			rgbd.AddForce (-speed, 0,0);
+
+		}
+		if (Input.GetKey (KeyCode.D) && grounded == true) {
+			rgbd.AddForce (-speed, 0,0);
+
+		}
+		if (Input.GetKeyDown (KeyCode.Space) && grounded == true) {
+			rgbd.AddForce (0, speed*2,0);
+
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		transform.position = new Vector3 (transform.position.x + 10 * Time.deltaTime, transform.position.y, transform.position.z);
-	
-	}
+
 }
